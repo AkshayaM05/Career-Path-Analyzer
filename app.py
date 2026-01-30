0641b7c6e78dc518b98f9b78a6c14f98e059f642
Akshaya M <akshayamv2005@gmail.com>
2026-01-30 14:50:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 import streamlit as st
 from pdfminer.high_level import extract_text
 import spacy
-import subprocess
-import sys
 
-try:
-    nlp = spacy.load("en_core_web_sm")
-except OSError:
-    subprocess.check_call([sys.executable, "-m", "spacy", "download", "en_core_web_sm"])
-    nlp = spacy.load("en_core_web_sm")
+
+nlp = spacy.load("en_core_web_sm")
 
 # Keywords for domain classification
 domain_keywords = {
@@ -158,4 +153,5 @@ elif option == "\U0001F4A1 Enter Area of Interest":
                 st.markdown(f"- {step}")
         else:
             st.warning("\u274C Sorry, we couldn't recognize that domain. Try keywords like: Web, Data, Cloud, DevOps, AI, etc.")
+
 
